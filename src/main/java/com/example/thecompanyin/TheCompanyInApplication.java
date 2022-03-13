e389df070ac2ad7ac295361f6c09d953eff6b551
Michal Marmorstein <michalmarmorstein@users.noreply.github.com>
2022-03-13 10:26:22 +0200

    initial commit

---
+++ src/main/java/com/example/thecompanyin/TheCompanyInApplication.java
@@ -0,0 +1,13 @@
+package com.example.thecompanyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCompanyInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCompanyInApplication.class, args);
+    }
+
+}
